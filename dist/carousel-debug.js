3548bf273304d0e95806249e6193dc28914b932f
seaui <seauijs@gmail.com>
2014-03-12 16:25:57 +0800

    build

---
+++ dist/carousel-debug.js
@@ -0,0 +1,153 @@
+define("seaui/carousel/1.0.0/carousel-debug", [ "jquery/jquery/1.10.1/jquery-debug", "seaui/transition/1.0.0/transition-debug", "./carousel-debug.css" ], function(require, exports, module) {
+    var $ = require("jquery/jquery/1.10.1/jquery-debug");
+    require("seaui/transition/1.0.0/transition-debug");
+    require("./carousel-debug.css");
+    "use strict";
+    // CAROUSEL CLASS DEFINITION
+    // =========================
+    var Carousel = function(element, options) {
+        this.$element = $(element);
+        this.$indicators = this.$element.find(".carousel-indicators");
+        this.options = options;
+        this.paused = this.sliding = this.interval = this.$active = this.$items = null;
+        this.options.pause == "hover" && this.$element.on("mouseenter", $.proxy(this.pause, this)).on("mouseleave", $.proxy(this.cycle, this));
+    };
+    Carousel.DEFAULTS = {
+        interval: 5e3,
+        pause: "hover",
+        wrap: true
+    };
+    Carousel.prototype.cycle = function(e) {
+        e || (this.paused = false);
+        this.interval && clearInterval(this.interval);
+        this.options.interval && !this.paused && (this.interval = setInterval($.proxy(this.next, this), this.options.interval));
+        return this;
+    };
+    Carousel.prototype.getActiveIndex = function() {
+        this.$active = this.$element.find(".item.active");
+        this.$items = this.$active.parent().children();
+        return this.$items.index(this.$active);
+    };
+    Carousel.prototype.to = function(pos) {
+        var that = this;
+        var activeIndex = this.getActiveIndex();
+        if (pos > this.$items.length - 1 || pos < 0) return;
+        if (this.sliding) return this.$element.one("slid.bs.carousel", function() {
+            that.to(pos);
+        });
+        if (activeIndex == pos) return this.pause().cycle();
+        return this.slide(pos > activeIndex ? "next" : "prev", $(this.$items[pos]));
+    };
+    Carousel.prototype.pause = function(e) {
+        e || (this.paused = true);
+        if (this.$element.find(".next, .prev").length && $.support.transition) {
+            this.$element.trigger($.support.transition.end);
+            this.cycle(true);
+        }
+        this.interval = clearInterval(this.interval);
+        return this;
+    };
+    Carousel.prototype.next = function() {
+        if (this.sliding) return;
+        return this.slide("next");
+    };
+    Carousel.prototype.prev = function() {
+        if (this.sliding) return;
+        return this.slide("prev");
+    };
+    Carousel.prototype.slide = function(type, next) {
+        var $active = this.$element.find(".item.active");
+        var $next = next || $active[type]();
+        var isCycling = this.interval;
+        var direction = type == "next" ? "left" : "right";
+        var fallback = type == "next" ? "first" : "last";
+        var that = this;
+        if (!$next.length) {
+            if (!this.options.wrap) return;
+            $next = this.$element.find(".item")[fallback]();
+        }
+        if ($next.hasClass("active")) return this.sliding = false;
+        var e = $.Event("slide.bs.carousel", {
+            relatedTarget: $next[0],
+            direction: direction
+        });
+        this.$element.trigger(e);
+        if (e.isDefaultPrevented()) return;
+        this.sliding = true;
+        isCycling && this.pause();
+        if (this.$indicators.length) {
+            this.$indicators.find(".active").removeClass("active");
+            this.$element.one("slid.bs.carousel", function() {
+                var $nextIndicator = $(that.$indicators.children()[that.getActiveIndex()]);
+                $nextIndicator && $nextIndicator.addClass("active");
+            });
+        }
+        if ($.support.transition && this.$element.hasClass("slide")) {
+            $next.addClass(type);
+            $next[0].offsetWidth;
+            // force reflow
+            $active.addClass(direction);
+            $next.addClass(direction);
+            $active.one($.support.transition.end, function() {
+                $next.removeClass([ type, direction ].join(" ")).addClass("active");
+                $active.removeClass([ "active", direction ].join(" "));
+                that.sliding = false;
+                setTimeout(function() {
+                    that.$element.trigger("slid.bs.carousel");
+                }, 0);
+            }).emulateTransitionEnd($active.css("transition-duration").slice(0, -1) * 1e3);
+        } else {
+            $active.removeClass("active");
+            $next.addClass("active");
+            this.sliding = false;
+            this.$element.trigger("slid.bs.carousel");
+        }
+        isCycling && this.cycle();
+        return this;
+    };
+    // CAROUSEL PLUGIN DEFINITION
+    // ==========================
+    var old = $.fn.carousel;
+    $.fn.carousel = function(option) {
+        return this.each(function() {
+            var $this = $(this);
+            var data = $this.data("bs.carousel");
+            var options = $.extend({}, Carousel.DEFAULTS, $this.data(), typeof option == "object" && option);
+            var action = typeof option == "string" ? option : options.slide;
+            if (!data) $this.data("bs.carousel", data = new Carousel(this, options));
+            if (typeof option == "number") data.to(option); else if (action) data[action](); else if (options.interval) data.pause().cycle();
+        });
+    };
+    $.fn.carousel.Constructor = Carousel;
+    // CAROUSEL NO CONFLICT
+    // ====================
+    $.fn.carousel.noConflict = function() {
+        $.fn.carousel = old;
+        return this;
+    };
+    // CAROUSEL DATA-API
+    // =================
+    $(document).on("click.bs.carousel.data-api", "[data-slide], [data-slide-to]", function(e) {
+        var $this = $(this), href;
+        var $target = $($this.attr("data-target") || (href = $this.attr("href")) && href.replace(/.*(?=#[^\s]+$)/, ""));
+        //strip for ie7
+        var options = $.extend({}, $target.data(), $this.data());
+        var slideIndex = $this.attr("data-slide-to");
+        if (slideIndex) options.interval = false;
+        $target.carousel(options);
+        if (slideIndex = $this.attr("data-slide-to")) {
+            $target.data("bs.carousel").to(slideIndex);
+        }
+        e.preventDefault();
+    });
+    $(window).on("load", function() {
+        $('[data-ride="carousel"]').each(function() {
+            var $carousel = $(this);
+            $carousel.carousel($carousel.data());
+        });
+    });
+});
+
+define("seaui/carousel/1.0.0/carousel-debug.css", [], function() {
+    seajs.importStyle(".carousel{position:relative}.carousel-inner{position:relative;overflow:hidden;width:100%}.carousel-inner>.item{display:none;position:relative;-webkit-transition:.6s ease-in-out left;transition:.6s ease-in-out left}.carousel-inner>.item>img,.carousel-inner>.item>a>img{display:block;max-width:100%;height:auto;line-height:1}.carousel-inner>.active,.carousel-inner>.next,.carousel-inner>.prev{display:block}.carousel-inner>.active{left:0}.carousel-inner>.next,.carousel-inner>.prev{position:absolute;top:0;width:100%}.carousel-inner>.next{left:100%}.carousel-inner>.prev{left:-100%}.carousel-inner>.next.left,.carousel-inner>.prev.right{left:0}.carousel-inner>.active.left{left:-100%}.carousel-inner>.active.right{left:100%}.carousel-control{position:absolute;top:0;left:0;bottom:0;width:15%;opacity:.5;filter:alpha(opacity=50);font-size:20px;color:#fff;text-align:center;text-shadow:0 1px 2px rgba(0,0,0,.6)}.carousel-control.left{background-image:-webkit-linear-gradient(left,color-stop(rgba(0,0,0,.5) 0),color-stop(rgba(0,0,0,.0001) 100%));background-image:linear-gradient(to right,rgba(0,0,0,.5) 0,rgba(0,0,0,.0001) 100%);background-repeat:repeat-x;filter:progid:DXImageTransform.Microsoft.gradient(startColorstr='#80000000', endColorstr='#00000000', GradientType=1)}.carousel-control.right{left:auto;right:0;background-image:-webkit-linear-gradient(left,color-stop(rgba(0,0,0,.0001) 0),color-stop(rgba(0,0,0,.5) 100%));background-image:linear-gradient(to right,rgba(0,0,0,.0001) 0,rgba(0,0,0,.5) 100%);background-repeat:repeat-x;filter:progid:DXImageTransform.Microsoft.gradient(startColorstr='#00000000', endColorstr='#80000000', GradientType=1)}.carousel-control:hover,.carousel-control:focus{outline:0;color:#fff;text-decoration:none;opacity:.9;filter:alpha(opacity=90)}.carousel-control .icon-prev,.carousel-control .icon-next,.carousel-control .glyphicon-chevron-left,.carousel-control .glyphicon-chevron-right{position:absolute;top:50%;z-index:5;display:inline-block}.carousel-control .icon-prev,.carousel-control .glyphicon-chevron-left{left:50%}.carousel-control .icon-next,.carousel-control .glyphicon-chevron-right{right:50%}.carousel-control .icon-prev,.carousel-control .icon-next{width:20px;height:20px;margin-top:-10px;margin-left:-10px;font-family:serif}.carousel-control .icon-prev:before{content:'\\2039'}.carousel-control .icon-next:before{content:'\\203a'}.carousel-indicators{position:absolute;bottom:10px;left:50%;z-index:15;width:60%;margin-left:-30%;padding-left:0;list-style:none;text-align:center}.carousel-indicators li{display:inline-block;width:10px;height:10px;margin:1px;text-indent:-999px;border:1px solid #fff;border-radius:10px;cursor:pointer;background-color:#000 \\9;background-color:rgba(0,0,0,0)}.carousel-indicators .active{margin:0;width:12px;height:12px;background-color:#fff}.carousel-caption{position:absolute;left:15%;right:15%;bottom:20px;z-index:10;padding-top:20px;padding-bottom:20px;color:#fff;text-align:center;text-shadow:0 1px 2px rgba(0,0,0,.6)}.carousel-caption .btn{text-shadow:none}@media screen and (min-width:768px){.carousel-control .glyphicons-chevron-left,.carousel-control .glyphicons-chevron-right,.carousel-control .icon-prev,.carousel-control .icon-next{width:30px;height:30px;margin-top:-15px;margin-left:-15px;font-size:30px}.carousel-caption{left:20%;right:20%;padding-bottom:30px}.carousel-indicators{bottom:20px}}");
+});
