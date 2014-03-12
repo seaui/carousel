ebe146ac01dd9611bd97430a1513459787586b61
seaui <seauijs@gmail.com>
2014-03-12 10:10:35 +0800

    init

---
+++ examples/index.md
@@ -0,0 +1,11 @@
+# Demo
+
+---
+
+## Normal usage
+
+````javascript
+seajs.use('carousel', function(carousel) {
+
+});
+````
---
+++ tests/carousel-spec.js
@@ -0,0 +1,12 @@
+define(function(require) {
+
+  var carousel = require('../src/carousel');
+
+  describe('carousel', function() {
+
+    it('normal usage', function() {
+
+    });
+  });
+
+});
